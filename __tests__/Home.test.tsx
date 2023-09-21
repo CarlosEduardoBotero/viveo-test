91c56a277580d0f3139bc2c6f4b51bacd0ecb849
Carlos Botero <boterocev@gmail.com>
2023-09-21 09:30:58 -0300

    Installed NextJs 13

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home", () => {
+  it("should have Docs text", () => {
+    render(<Home />); // ARRANGE
+
+    const myElem = screen.getByText("carlos"); // ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+});
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  type Test2 = {};
+  const test = "ttee";
+  return <div>carlos</div>;
+}
